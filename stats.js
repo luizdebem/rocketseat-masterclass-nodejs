681c2946b6a108ad66222cd3831b571fbbcbb8e1
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-04-08 21:24:12 -0300

    logger

--- stats.js
+++ stats.js
@@ -1,11 +1,13 @@
-const os = require('os');
+sconst os = require('os');
+const log = require('./logger');
 const { freemem, totalmem } = os;
+
 setInterval(() => {
   const total = parseInt(totalmem() / 1024 / 1024);
   const mem = parseInt(freemem() / 1024 / 1024);
   const percents = parseInt((mem / total) * 100);
 
-  const stats = {
+  const stats = {s
     free: `${mem} MB`,
     total: `${total} MB`,
     usage: `${percents}%`
@@ -13,4 +15,6 @@ setInterval(() => {
   console.clear();
   console.log('- - - MEMORY STATS - - -');
   console.table(stats);
+
+  log(`${JSON.stringify(stats)} -> ${Date()}\n`);
 }, 1000);
